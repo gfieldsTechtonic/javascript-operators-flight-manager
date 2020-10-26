0879dff8988f84d7bc42da0cc3442e2ffee46886
Grant Fields Techtonic <grant.fields@techtonic.com>
2020-10-26 16:10:38 -0400

    Second part testing

--- logic/flights.js
+++ logic/flights.js
@@ -1,7 +1,34 @@
 function Flights() {
 
     function calculateNumberOfFlights(numberOfPassengers, flightCapacity){
-        return Math.ceil(numberOfPassengers / flightCapacity);
+
+        var errorMsg;
+
+        try{
+
+            if(numberOfPassengers < 0){
+                errorMsg = "The number of passengers must be a positive integer value";
+                throw error;
+            }
+            else if(flightCapacity < 0){
+                errorMsg = "The capacity of the flight must be a positive integer value";
+                throw error;
+            }
+            else{
+                return Math.ceil(numberOfPassengers / flightCapacity);
+            }
+        }
+        catch(error){
+
+            console.log(errorMsg);
+        }
+        finally{
+
+            console.log(numberOfPassengers);
+            console.log(flightCapacity);
+        }
+
+        
     }
 
     return{calculateNumberOfFlights};
