a86cdcb6be4359dc2145fa3e4d230b1d96b7f132
Grant Fields Techtonic <grant.fields@techtonic.com>
2020-10-26 18:00:32 -0400

    6th part testing

--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,18 @@
 function Util() {
 
+    function calculateTotalDistributedPassengers(seatList){
+
+        var sum = 0;
+
+        for(var x in seatList){
+
+            sum += x;
+        }
+
+        return sum;
+    }
+
+    return {calculateTotalDistributedPassengers};
 }
 
+module.exports = Util();
