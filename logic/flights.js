4e2038f8d1a28a15acae243f3dda84b825da314a
Grant Fields Techtonic <grant.fields@techtonic.com>
2020-10-26 16:43:41 -0400

    Third part testing

--- logic/flights.js
+++ logic/flights.js
@@ -13,7 +13,40 @@ function Flights() {
         }
     }
 
-    return{calculateNumberOfFlights};
+    function checkAircraftRevision(distanceLimit, distances){
+
+        var outMsg = "";
+
+        var sum = 0;
+
+        for(var i = 0; i < distances.length; i++){
+
+            sum += distances[i];
+        }
+
+        sum /= distanceLimit;
+
+        if(sum > 1){
+
+            throw new Error("");
+        }
+        else if(sum >= .75){
+
+            outMsg = "The revision needs to be done within the next month";
+        }
+        else if(sum >= .5){
+
+            outMsg = "The revision needs to be done within the next 2 months";
+        }
+        else{
+
+            outMsg = "The revision needs to be done within the next 3 months";
+        }
+
+        return outMsg;
+    }
+
+    return{calculateNumberOfFlights, checkAircraftRevision};
 }
 
 module.exports = Flights();
