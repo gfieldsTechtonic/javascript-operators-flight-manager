4390405e238a6f603781dc6411923b0dc0d9d4e5
Grant Fields Techtonic <grant.fields@techtonic.com>
2020-10-26 17:27:56 -0400

    5th part testing

--- logic/passengers.js
+++ logic/passengers.js
@@ -16,7 +16,80 @@ function Passengers() {
         return sum;
     }
 
-    return {checkFlightCapacity};
+    function distributeAllSeatsToAllPassengers(vip, reg, numOfFlights, businessSeats, econSeats){
+        
+        businessSeats *= numOfFlights;
+        econSeats *= numOfFlights;
+
+        vipBusinessTemp = 0;
+        vipEconTemp = 0;
+        regBusinessTemp = 0;
+        regEconTemp = 0;
+
+        if(vip < businessSeats){
+
+            vipBusinessTemp += vip;
+            businessSeats -= vip;
+            vip = 0;
+        }
+        else{
+
+            vipBusinessTemp = businessSeats;
+            vip -= businessSeats;
+            businessSeats = 0;
+
+            if(vip < econSeats){
+                
+                vipEconTemp += vip;
+                econSeats -= vip;
+                vip = 0;
+            }
+            else{
+
+                vipEconTemp = econSeats;
+                vip -= econSeats;
+                econSeats = 0;
+            }
+        }
+
+        if(reg < businessSeats){
+
+            regBusinessTemp += reg;
+            businessSeats -= reg;
+            vip = 0;
+        }
+        else{
+
+            regBusinessTemp = businessSeats;
+            reg -= businessSeats;
+            businessSeats = 0;
+
+            if(reg < econSeats){
+                
+                regEconTemp += reg;
+                econSeats -= reg;
+                vip = 0;
+            }
+            else{
+
+                regEconTemp = econSeats;
+                reg -= econSeats;
+                econSeats = 0;
+            }
+        }
+
+        var seatList = {
+
+            vipBusiness: vipBusinessTemp,
+            vipEcon: vipEconTemp,
+            regBusiness: regBusinessTemp,
+            regEcon: regEconTemp
+        };
+
+        return seatList;
+    }
+
+    return {checkFlightCapacity, distributeAllSeatsToAllPassengers};
 }
 
 module.exports = Passengers();
