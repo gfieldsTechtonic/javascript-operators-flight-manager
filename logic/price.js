c3efca7200521561879276db5c42b6c42b86f0f6
Grant Fields Techtonic <grant.fields@techtonic.com>
2020-10-26 18:53:31 -0400

    Commiting prices function

--- logic/price.js
+++ logic/price.js
@@ -1,4 +1,17 @@
 function Prices() {
+    function calculateFinalPrice(base, varPassenger, varFlight){
 
+        var price = base;
+
+        price = price + (varPassenger/100 * price);
+        price = price + (varFlight/100 * price);
+
+        price = Math.round(price * 100) / 100;
+
+        return price;
+    }
+
+    return {calculateFinalPrice};
 }
 
+module.exports = Prices();
