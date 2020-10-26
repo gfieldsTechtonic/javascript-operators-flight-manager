7e090268fdae0693c8bfee636b147e9399bb9bfe
Grant Fields Techtonic <grant.fields@techtonic.com>
2020-10-26 16:51:01 -0400

    4th part testing

--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,19 @@
 function Passengers() {
 
+    function checkFlightCapacity(flightCapacity, passengers){
+
+        var sum = 0;
+
+        for(var i = 0; i < passengers.length; i++){
+
+            sum += passengers[i];
+        }
+
+        if(flightCapacity < passengers){
+            throw new Error("");
+        }
+
+        return sum;
+    }
 }
 
